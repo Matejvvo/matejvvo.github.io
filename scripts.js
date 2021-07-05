92f703e64accba4aa823613152b56001daddf483
Matejvvo <77694508+Matejvvo@users.noreply.github.com>
2021-07-05 20:05:47 +0200

    New page

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
+// Function expresion to select elements --- lambda
+const selectElement = (s) => document.querySelector(s);
+
+// Open menu on click
+selectElement(".open").addEventListener("click", () => {
+    selectElement(".nav-list").classList.add("active");
+})
+
+// Close menu on click
+selectElement(".close").addEventListener("click", () => {
+    selectElement(".nav-list").classList.remove("active");
+})
+
 // Detect scrolling
 window.onscroll = function(e) {
-    document.getElementById("id").classList.add("class");
+    document.getElementById("top-btn").classList.add("active");
     setTimeout(function() {
-        document.getElementById("id").classList.remove("class");
-    }, 1111);
+        document.getElementById("top-btn").classList.remove("active");
+    }, 3000);
 }
